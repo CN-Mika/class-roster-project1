dc783231a838b3507c90ba3d202849f2ec6f6851
masalaza-cn <83977863+masalaza-cn@users.noreply.github.com>
2021-12-13 21:13:45 -0500

    created objects, mapped arrays

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import "./styles.css";
 
-function createRosterItem(studentName, studentPronouns, studentHS) {
+function createRosterItem(name, pronouns, hS) {
   let rosterItem = (
-    <div className="studentRow">
-      <div className="studentData name">{studentName}</div>
-      <div className="studentData pronouns">{studentPronouns}</div>
-      <div className="studentData highschool">{studentHS}</div>
+    <div className="row">
+      <div className="data name">{name}</div>
+      <div className="data pronouns">{pronouns}</div>
+      <div className="data highschool">{hS}</div>
     </div>
   );
   return rosterItem;
 }
 
+let students = [
+  {
+    name: "Mikkail Allen",
+    pronouns: "He/His",
+    hS: "Williamsburg Charter Highschool"
+  },
+  { name: "Justin Apupalo", pronouns: "He/His", hS: "NYIT" },
+  {
+    name: "Christopher Bonifacio",
+    pronouns: "He/His",
+    hS: "Orchard Collegiate Academy"
+  }
+];
+
+let classRoster = [];
+
+classRoster = students.map(function (student) {
+  return createRosterItem(student.name, student.pronouns, student.hS);
+});
+
 export default function App() {
   return (
     <div className="App">
       <h1>Code Nation Roster</h1>
-      {createRosterItem(
-        "Mikkail Allan",
-        "He/His",
-        "Williamsburg Charter Highschool"
-      )}
-      {createRosterItem("Justin Apupalo", "He/His", "NYIT")}
-      {createRosterItem(
-        "Christopher Bonifacio",
-        "He/His",
-        "Orchard Collegiate Academy"
-      )}
+      {classRoster}
     </div>
   );
 }
